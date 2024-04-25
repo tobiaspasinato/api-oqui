e4af12410ad4150fd3094a9ea79d662ddee6dfcd
tobiaspasinato <tobiasmarpasinato2005@gmail.com>
2024-04-24 21:55:46 -0300

    0,gk

--- main.py
+++ main.py
@@ -3,32 +3,6 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class Alonso(BaseModel):
-    nombre: str
-    apellido: str
-    edad: int
-    dni: int
-
 @app.get("/")
 def index():
     return {"msj" : "Hola soy un Ñoqui"}
-
-@app.get("/getinfo")
-def mostrar_info(Persona : Alonso):
-    return {"Piloto" : f"Nombre : {Persona.nombre},\nApellido : {Persona.apellido}"}
-
-@app.get("/sumar")
-def sumar(a: int, b: int):
-    return a + b
-
-@app.get("/restar")
-def restar(a: int, b: int):
-    return a - b
-
-@app.get("/multiplicar")
-def multiplicar(a: int, b: int):
-    return a * b
-
-@app.get("/dividir")
-def dividir(a: int, b: int):
-    return a / b
